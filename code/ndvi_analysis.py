7aab2ca159a0d296eb21469474b1f95551d4b9cd
Mark Skilbeck <markskilbeck@gmail.com>
2018-04-24 16:59:28 +0100

    Add calibration comparision stuff

--- code/ndvi_analysis.py
+++ code/ndvi_analysis.py
@@ -45,6 +45,27 @@ saved as numpy array into configured threshold_dir and threshold_fmt."""
     return None
 
 for year in np.arange(2009, 2018):
+def calibration_comparison(year, month, region):
+    # Try load NDVI data
+    ndvi_uncal = np.load(ndvi_dir + ndvi_fmt.format(year, month, region) + "_uncalibrated.npy")
+    ndvi_cal   = np.load(ndvi_dir + ndvi_fmt.format(year, month, region) + ".npy")
+
+    import matplotlib.pyplot as plt
+    import matplotlib.colors as clr
+    cmap = clr.LinearSegmentedColormap.from_list('', ['red', 'white', 'darkgreen'])
+    f, (ax1, ax2) = plt.subplots(1, 2, sharey=True, figsize=(10,4.5), dpi=150)
+    ax1.set_title('Uncalibrated NDVI for {} {}, {}'.format(region.capitalize(), month, year))
+    ax1.axis('off')
+    plot1 = ax1.imshow(ndvi_uncal, cmap=cmap, vmin=-np.max(ndvi_uncal), vmax=np.max(ndvi_uncal))
+    ax2.set_title('Calibrated NDVI for {} {}, {}'.format(region.capitalize(), month, year))
+    ax2.axis('off')
+    plot2 = ax2.imshow(ndvi_cal, cmap=cmap, vmin=-np.max(ndvi_cal), vmax=np.max(ndvi_cal))
+    f.subplots_adjust(right=0.8)
+    cb_ax = f.add_axes([0.85, 0.15, 0.05, 0.7])
+    f.colorbar(plot2, cax=cb_ax)
+    plt.savefig('test.pdf')
+
+    return None
     for region in ('capetown', 'eastafrica'):
         print('Producing NDVI data for {} {}'.format(region, year))
         ndvi_for_year_and_region(year, region)
