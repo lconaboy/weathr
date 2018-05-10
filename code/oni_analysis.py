5e4d6203ac3d3dc6bd55a0156478af36c51ef9e3
lconaboy <lukeconaboy@hotmail.co.uk>
2018-05-10 17:30:23 +0100

    added plot comparing rainfall and cloud fraction

--- code/oni_analysis.py
+++ code/oni_analysis.py
@@ -11,7 +11,7 @@ import scipy.stats
 """Mark: to use this, just replace cf with ndvi data."""
 
 regions = ['capetown', 'eastafrica']
-east_or_south = 1  # 0 for south, 1 for east
+east_or_south = 0  # 0 for south, 1 for east
 region = regions[east_or_south]
 start = datetime.datetime.strptime('20081','%Y%m')
 end = datetime.datetime.strptime('201712','%Y%m')
@@ -33,18 +33,6 @@ wtio_tmm = swio_three_monthly_means(wtio_datetime, wtio_anoms)
 dmi_datetime, dmi_anoms = load_swio('dmi.nc', 'DMI')
 dmi_tmm = swio_three_monthly_means(dmi_datetime, dmi_anoms)
 
-# # now rainfall
-# fname = 'eth_rainfall_1991_2015.txt'
-# raw_rf = np.loadtxt(fname, skiprows=1, usecols=(0, 1, 2))
-# # need to convert to list format to use with yearly_mean, what a bore
-# # yearly_mean does not play well with rainfall data
-# rf = [[x for x in raw_rf[:, y]] for y in [1, 2, 0]]
-# rf_means = yearly_mean(rf, err=False)
-# # now we need to narrow rf to the range of our data
-# rf_narr = narrow_to_range(rf, start, end)
-# rf_anoms = cloud_fraction_anomalies(rf_narr, rf_means, start, end, err=False)
-# rf_anoms_smoothed = smooth_data_with_window(rf_anoms[3], step)
-
 # load the anomaly data and narrow to range
 # match the central anomaly months to the smoothed data (i.e. if the
 # data is smoothed with a window of 2, then start is actually 2 months
@@ -52,7 +40,8 @@ dmi_tmm = swio_three_monthly_means(dmi_datetime, dmi_anoms)
 # step-1 accounts for the ONI being smoothed with a step of 1
 new_start, new_end = adjust_dates_to_smoothed_range(start, end, step-1)
 # shift the dates to check for correlation
-# shifted_start, shifted_end = shift_dates(new_start, new_end, shift)
+# shift = -2
+# new_start, new_end = shift_dates(new_start, new_end, shift)
 nino = nino_range('detrend.nino34.ascii.txt', new_start, new_end)
 # pick out consecutive anomalies
 oni_anoms = consecutive_anomalies(nino, new_start, new_end)
@@ -105,8 +94,8 @@ plt.title('{}'.format(region_to_string(region)))
 plt.axhline(linewidth=0.75, color='k')
 plt.axhline(y=0.5, linewidth=0.75, color='k', linestyle='dashed')
 plt.axhline(y=-0.5, linewidth=0.75, color='k', linestyle='dashed')
-plt.savefig(figure_dir + 'dmi_' + narrowed_io[1][east_or_south] +
-            '_{}_{}window'.format(region, 2*step+1))
+# plt.savefig(figure_dir + 'dmi_' + narrowed_io[1][east_or_south] +
+#            '_{}_{}window'.format(region, 2*step+1))
 
 # now plot the ONI (El Nino)
 corr_labels = ['CF', 'ONI']
@@ -116,40 +105,61 @@ plt.title('{}'.format(region_to_string(region)))
 plt.axhline(linewidth=0.75, color='k')
 plt.axhline(y=0.5, linewidth=0.75, color='k', linestyle='dashed')
 plt.axhline(y=-0.5, linewidth=0.75, color='k', linestyle='dashed')
-plt.savefig(figure_dir + 'oni_' + narrowed_io[1][east_or_south] +
-            '_{}_{}window'.format(region, 2*step+1))
+# plt.savefig(figure_dir + 'oni_' + narrowed_io[1][east_or_south] +
+#            '_{}_{}window'.format(region, 2*step+1))
 
 
-plt.figure()
+plt.figure(figsize=(4,4))
 plt.hist((cf_anoms_smoothed[0][~en&~ln], cf_anoms_smoothed[0][en],
-          cf_anoms_smoothed[0][ln]))
-plt.legend([r'Neutral', 'El Ni$\{~}$no', 'La Ni$\{~}$na'])
-plt.show()
+          cf_anoms_smoothed[0][ln]), range=([-0.6, 0.6]))
+plt.legend([r'Neutral', r'El Ni$\mathrm{\tilde{n}}$o', r'La Ni$\mathrm{\tilde{n}}$a'])
+plt.title(region_to_string(region))
+plt.xlabel(r'CF$_{\sigma}$')
+# plt.savefig(figure_dir + 'cf_anoms_hist_' + region + 'oni_shift)
 
-# fig, axes = plt.subplots(2, 3, figsize=(12, 8), sharex=False, sharey=False)
-# titles = ['EN', 'LN', 'Neutral']
-# pos = [en, ln, ~en&~ln]
-# for idx, ax in enumerate(axes.ravel()[0:3]):
-#     ax.set_title(titles[idx])
-#     x = cf_anoms_smoothed[0][pos[idx]]
-#     histo = ax.hist(x)
-# for idx, ax in enumerate(axes.ravel()[3:6]):
-#     x = cf_anoms_smoothed[0][pos[idx]]
-#     kernel = stats.gaussian_kde(x)
-#     histo = np.histogram(x)
-#     prob = np.zeros(len(histo[1])-1)
-#     for i in range(0, len(histo[1])-1):
-#         x_range = np.linspace(histo[1][i], histo[1][i+1], 100)
-#         y = kernel(x_range)
-#         area = np.trapz(y, x_range)
-#         prob[i] = area
-#     f_exp = sum(histo[0])*prob
-#     chi_s = stats.chisquare(histo[0], f_exp)
-#     ax.plot(np.linspace(min(x), max(x)),
-#             kernel(np.linspace(min(x), max(x))))
-#     ax.text(0.6, 0.75, r'$\chi^2={:3f}$'.format(chi_s[0]), transform=ax.transAxes)
-#     ax.set_xlabel(r'CF$_{\sigma}$')
+# now rainfall
+end_rainfall = datetime.datetime.strptime('201512','%Y%m')
+fname = ['sa_rainfall_1991_2015.txt', 'eth_rainfall_1991_2015.txt']
+raw_rf = np.loadtxt(fname[east_or_south], skiprows=1, usecols=(0, 1, 2))
+# need to convert to list format to use with yearly_mean, what a bore
+# yearly_mean does not play well with rainfall data
+rf = [[x for x in raw_rf[:, y]] for y in [1, 2, 0]]
+rf_means = yearly_mean(rf, err=False)
+# now we need to narrow rf to the range of our data
+rf_narr = narrow_to_range(rf, start, end_rainfall)
+rf_anoms = cloud_fraction_anomalies(rf_narr, rf_means, start, end_rainfall, err=False)
+rf_anoms_smoothed = smooth_data_with_window(rf_anoms, step, err=False)
+cf = load_cloud_fraction_period(start, end_rainfall, region)
+cf_means = yearly_mean(cf)
+cf_anoms = cloud_fraction_anomalies(cf, cf_means, start, end_rainfall)
+cf_anoms_smoothed = smooth_data_with_window(cf_anoms, step)
+legends = ['CF', 'RF']
 
-plt.suptitle(r'{} $n={}$'.format(region_to_string(region), sum(histo[0])))
-plt.savefig('hist_with_pdf_and_chi{}'.format(region))
-plt.show()
+month_step = 6
+month_labels = cf_anoms[1][::month_step]
+year_labels = np.arange(min(cf_anoms[0]), max(cf_anoms[0])+1)
+x_labels = month_and_year_labels(month_labels, year_labels, month_step)
+
+def plot_rainfall_and_cloud(cf_anoms_smoothed, rf_anoms_smoothed, legends, x_labels):
+    fig, ax1 = plt.subplots(figsize=(5.2, 5))
+    plt.axhline(linewidth=1, color='k')
+    ax1.plot(cf_anoms_smoothed[0], label=legends[0])
+    ax1.set_ylim([-0.6, 0.6])
+    ax1.set_ylabel(legends[0]+r'$_{\sigma}$')
+    ax2 = ax1.twinx()
+    ax2.plot(rf_anoms_smoothed, label=legends[1], color='r')
+    ax2.set_ylabel(legends[1]+r'$_{\sigma}$')
+    ax2.set_ylim([-0.8, 0.8])
+    lines1, labels1 = ax1.get_legend_handles_labels()
+    lines2, labels2 = ax2.get_legend_handles_labels()
+    ax1.legend(lines1 + lines2, labels1 + labels2, loc=2)
+    ax1.set_xticks([])
+    ax2.set_xticks(np.linspace(ax1.get_xbound()[0], ax1.get_xbound()[1], len(x_labels)))
+    ax2.set_xticklabels(x_labels)
+    ax1.set_xlabel('Months')
+    ax2.set_xlabel('Months')
+    plt.title(region_to_string(region))
+    plt.tight_layout()
+    plt.savefig('rf_cf_anomalies_{}'.format(region))
+
+plot_rainfall_and_cloud(cf_anoms_smoothed, rf_anoms_smoothed, legends, x_labels)
